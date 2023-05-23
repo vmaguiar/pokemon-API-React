77099e7de48b3c5dd210444781594c5478781e73
vmaguiar <vmaguiar@hotmail.com>
2023-05-23 18:50:54 -0300

    feat: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+/*
+Consume the API and list all pokemons of the resquested endpoint.
+https://pokeapi.co/api/v2/pokemon
+
+You must show of each pokemon:
+- image
+- name
+- exp
+
+You can acess the information of a single pokemon at:
+https://pokeapi.co/api/v2/pokemon/:id
+
+
+HINT:
+image => sprites.front_default
+exp => base_experience
+
+BONUS: If you can order by name.
+*/
+
+
+import './App.css'
+
+export function App() {
+
+  return (
+    <>
+      <h1>Initial commit</h1>
+    </>
+  )
+}
+
