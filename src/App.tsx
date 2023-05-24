d6125e3733d5cbaee1fd3abfd5f68ebad749286f
vmaguiar <vmaguiar@hotmail.com>
2023-05-24 11:59:54 -0300

    feat: Load api to a state and create h1 o each obj

--- src/App.tsx
+++ src/App.tsx
@@ -19,30 +19,48 @@ BONUS: If you can order by name.
 */
 
 
-import axios from 'axios'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-import './App.css'
+import './App.css';
+
+
 
 const baseURL = 'https://pokeapi.co/api/v2/pokemon';
 
+interface IpokemonObj {
+  name: string,
+  url: string
+};
+
+
 export function App() {
+  const [pokemonList, setPokemonList] = useState<Array<IpokemonObj>>([]);
 
-  const testandoAxios = async () => {
-    try {
-      const response =  await axios.get(baseURL);
-      console.log(response);
-    }
-    catch(error) {
-      console.log(error);
-    }
-  }
 
-  testandoAxios();
+  useEffect(() => {
+    //Create the API request as scoped async function in the hook
+    async function loadApiData() {
+      try {
+        const response =  await axios.get(baseURL);
+        setPokemonList(response.data.results);
+      }
+      catch(error) {
+        console.log(error);
+      } 
+    }
+
+    loadApiData();
+  }, []);
 
   return (
     <>
-      <h1>Import & Testing API with axios</h1>
+      {
+        pokemonList.map((itemObj, index) => (
+          <h1 key={index}>{index} {itemObj.name}</h1>
+        ))
+      }
     </>
-  )
+  );
 }
 
